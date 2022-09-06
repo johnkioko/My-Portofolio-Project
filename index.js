5df6cddbcfbe008896ed30ee531307156143920e
John <kiokoj81@gmail.com>
2022-09-06 12:34:24 +0300

    Made Css changes to hamburger

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let hamburger = document.querySelector('.hamberg');
-let navMenu = document.querySelector('.menu-list');
+let navMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener("click", function(){
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");   
 })
 
-document.querySelectorAll(".menu-list-link").forEach(link => link.addEventListener("click", function(){
+document.querySelectorAll(".menu-list-link").forEach(linkItem => linkItem.addEventListener("click", function(){
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");  
 }))
